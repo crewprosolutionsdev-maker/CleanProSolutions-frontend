2021799a492f4a908c4004a3cb4ae7921e3df5e6
Leandro Pelozo <crewpro.solutions.dev@gmail.com>
2026-05-31 13:05:41 -0300

    Actualizacion de URL para conectar con backend

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -42,7 +42,7 @@ function ContactPage() {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:3000/api/contact', {
+            const response = await fetch(`${API_URL}/api/contact`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
